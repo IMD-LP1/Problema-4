25f2a110e6dcbe6886d0d3b1fe613e1a6f55a879
Fernando Rodrigues Maciel <fernandomaciel@A307-33L.imd.local>
2017-09-12 19:03:43 -0300

    Problema-4

---
+++ lists/list3103.cpp
@@ -0,0 +1,30 @@
+/** @file list3103.cpp */
+/** Listing 31-3. Using a Class and its Members */
+#include <iostream>
+#include <ostream>
+
+struct point
+{
+	double x;
+	double y;
+};
+
+int main()
+{
+	point origin{}, unity{};
+
+	origin.x = 0;
+	origin.y = 0;
+	unity.x = 0;
+	unity.y = 0;
+
+	std::cout << "origin = (" << origin.x << ", " << origin.y << ")\n";
+	std::cout << "origin = (" << unity.x << ", " << unity.y << ")\n";
+	
+}
+
+
+/** E cahamado o inicializador padraão mas eles não são inicializado não tendo valor
+
+esse campos são membros de dados e atributos
+*/
